ccbc572c79adfee8b1a7885d89578a5732883789
Smriti <20bcs105@nith.ac.in>
2021-09-28 18:52:18 +0530

    added alumni

--- src/components/alumni/alumni.jsx
+++ src/components/alumni/alumni.jsx
@@ -1,79 +1,46 @@
 import React, { Component } from 'react';
 import Cards from '../UI/Alumni_cards/Cards';
-
 import axios from 'axios';
-
 import './alumni.css';
-class Alumni extends Component{
-  state={
-    dummy:[]
+
+class Alumni extends Component {
+  state = {
+    dummy: []
   }
-  componentDidMount(){
+  componentDidMount() {
     axios.get('https://spec-backend.herokuapp.com/api/alumni/')
-    .then(response => {
-      console.log(response.data)
-      this.setState({dummy:response.data});
-    })
-    .catch(err => {
-      console.log(err)
-    })
+      .then(response => {
+        console.log(response.data)
+        this.setState({ dummy: response.data });
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 
-  render(){
+  render() {
     return (
       <>
-      <header className="bg-white shadow">
+        <header className="bg-white shadow">
           <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
             <h1 className="text-6xl font-bold text-gray-900 text-center">OUR ALUMNI</h1>
           </div>
-          
-          </header>
-          <div className="batch">Batch 2020</div>
-      <div className="grid md:grid-cols-2 gap-y-2 sm:grid-cols-1">
-      {this.state.dummy.map(test=>(<Cards
-        name={test.name}
-        key={test.name}
-        imgsrc={test.imgsrc}
-        company={test.company}
-        batch={test.batch}
-        link={test.link}
-      />))}
-      </div>
-      <div className="batch">Batch 2019</div>
-      <div className="grid md:grid-cols-2 gap-y-2 sm:grid-cols-1">
-      {this.state.dummy.map(test=>(<Cards
-        name={test.name}
-        key={test.name}
-        imgsrc={test.imgsrc}
-        company={test.company}
-        batch={test.batch}
-        link={test.link}
-      />))}
-      </div>
-      <div className="batch">Batch 2018</div>
-      <div className="grid md:grid-cols-2 gap-y-2 sm:grid-cols-1">
-      {this.state.dummy.map(test=>(<Cards
-        name={test.name}
-        key={test.name}
-        imgsrc={test.imgsrc}
-        company={test.company}
-        batch={test.batch}
-        link={test.link}
-      />))}
-      </div>
-      <div className="batch">Batch Before 2018</div>
-      <div className="grid md:grid-cols-2 gap-y-2 sm:grid-cols-1">
-      {this.state.dummy.map(test=>(<Cards
-        name={test.name}
-        key={test.name}
-        imgsrc={test.imgsrc}
-        company={test.company}
-        batch={test.batch}
-        link={test.link}
-      />))}
-      </div>
-      
-       
+        </header>
+        {["2020", "2019", "2018"].map(xyz => (
+          <div>
+            <div className="batch">{xyz}</div>
+            <div className="grid md:grid-cols-2 gap-y-2 sm:grid-cols-1">
+              {this.state.dummy.map(test => (<Cards
+                name={test.name}
+                key={test.name}
+                imgsrc={test.profile_pic}
+                company={test.company}
+                batch={test.year}
+                link={test.linkedin_id}
+              />))}
+            </div>
+          </div>
+        ))}
       </>
     );
   }
